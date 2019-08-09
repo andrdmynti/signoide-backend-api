b8aa527e93d44555303ba4773d28a70a9f9d0e1b
Andrea Damayanti <andr.dmynti22@gmail.com>
2019-08-09 17:01:02 +0700

    update login

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,17 @@
 |
 */
 
-// Route::get('/dashboard', function () {
-//     return view('admin.dashboard');
-// });
-
-Route::get('/noide-admin/', function () {
+Route::get('/noide-admin', function () {
     return view('admin.login');
 });
 
 Auth::routes();
 
-// Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/dashboard', 'HomeController@index')->name('dashboard.index');
+Route::get('/home', 'HomeController@index')->name('dashboard.index')->middleware('auth');
+
+Route::prefix('bank')->group(function(){
+    Route::get('list', 'Bank\Controller\BankController@index')->name('bank.list');
+    Route::get('create', 'Bank\Controller\BankController@create')->name('bank.create');
+    Route::post('store', 'Bank\Controller\BankController@store')->name('bank.store');
+    Route::get('status', 'Bank\Controller\BankController@status')->name('bank.status');
+});
