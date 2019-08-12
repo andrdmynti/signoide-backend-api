ed4829a979d0a26b5e883666e9e1f85e18ff5ccd
Andrea Damayanti <andr.dmynti22@gmail.com>
2019-08-12 15:47:30 +0700

    adding views show data detail bank

--- resources/views/bank/show.blade.php
+++ resources/views/bank/show.blade.php
@@ -5,6 +5,31 @@
     </div>
     <div class="card shadow mb-4">
         <div class="card-body">
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Nama Bank</th>
+                        <td>:</td>
+                        <td>{{ $bank->nama_bank}}</td>
+                    </tr>
+                    <tr>
+                        <th>No. Rekening</th>
+                        <td>:</td>
+                        <td>{{ $bank->no_rek }}</td>
+                    </tr>
+                    <tr>
+                        <th>Atas Nama Kepemilikan</th>
+                        <td>:</td>
+                        <td>{{ $bank->atas_nama}}</td>
+                    </tr>
+                    <tr>
+                        <th>Cabang</th>
+                        <td>:</td>
+                        <td>{{ $bank->cabang}}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <br>
         </div>
     </div>
 @endsection
